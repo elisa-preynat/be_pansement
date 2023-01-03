1b94bf8c056c122de10aba9a36fee90fb3f6b309
elisa.preynat <elisa.preynat@isen.yncrea.fr>
2023-01-03 16:42:45 +0100

    add files

---
+++ app/src/main/java/fr/isen/preynat/myapplication/ConnectionType.kt
@@ -0,0 +1,11 @@
+package fr.isen.preynat.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConnectionType : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_connection_type)
+    }
+}
